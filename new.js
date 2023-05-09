e7c13683296f1c3ee886bb2cd33c36c37ef0b40a
mk8bk <moncef.aitbelkacem@gmail.com>
2023-05-09 05:21:37 +0200

    clened css and improved html semantics

--- new.js
+++ new.js
@@ -32,16 +32,10 @@ function flipTheme(){
         body.classList.remove('dark-body-theme');
         body.classList.add('light-body-theme');
         localStorage.setItem('portfolio-theme','light');
-        hamburgerButton.style.backgroundImage = 'url("data/icons/menu-dark.png")';
-        themeButton.style.backgroundImage = 'url("data/icons/theme-dark.png")';
-        console.log('set light theme')
     }else{
         body.classList.remove('light-body-theme');
         body.classList.add('dark-body-theme');
         localStorage.setItem('portfolio-theme','dark');
-        hamburgerButton.style.backgroundImage = 'url("data/icons/menu-light.png")';
-        themeButton.style.backgroundImage = 'url("data/icons/theme-light.png")';
-        console.log('set dark theme')
     }
 }
 
@@ -62,9 +56,7 @@ function initTheme(){
  * Show the links drop down men.
  */
 function toggleDropDown(){
-    console.log(dropDown);
     if(dropDown.classList.contains('active')){
-        console.log("hiding");
         hideDropDown();
     }else{
         dropDown.classList.toggle('active');
